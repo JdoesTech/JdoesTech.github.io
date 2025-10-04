98b4d6644414042f43f2e50b64b93936046f5b00
JdoesTech <onyangoocholla1139@gmail.com>
2025-10-04 18:12:01 +0300

    Nothing actually

---
+++ script.js
@@ -0,0 +1,8 @@
+const theme = document.getElementbyId("theme-toggle");
+theme.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+});
+
+function navControls(){
+    const nav = document.querySelector("nav");
+}
